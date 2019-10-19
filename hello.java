7d5a7dfd0d3a4b8a2bc108b34e0957d3a2beacd3
Sergio Armenta <sergioarmenta888@gmail.com>
2019-10-19 08:53:56 -0400

    changed hello world file

--- hello.java
+++ hello.java
@@ -5,10 +5,10 @@ public class hello {
     public static void main(String [] args) {
 
 
-        String firstName = "Elizabeth";
-        String lastName = "Jones";
+        String firstName = "Sergio";
+        String lastName = "Armenta";
 	
-        System.out.println("Hello Class!");
+        System.out.println("Hello, my name is " + firstName + " " + lastName);
 
 
 
